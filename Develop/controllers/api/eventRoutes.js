dd7351bb5e19d806e03871895c1e18ae6486c9d7
Benjamin Clewer <benjiclewer@gmail.com>
2021-12-09 19:25:44 +1030

    adding authentication to routes

--- Develop/controllers/api/eventRoutes.js
+++ Develop/controllers/api/eventRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Event } = require('../../models');
+const withAuth = require('../../../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {

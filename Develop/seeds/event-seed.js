b96d10d3961f8b1c683bae0c0ba47fa0b4832d43
Carol Shiers <carol.shiers@gmail.com>
2021-12-11 09:53:54 +1000

    adding category id to event seeding

--- Develop/seeds/event-seed.js
+++ Develop/seeds/event-seed.js
@@ -1,6 +1,6 @@
 // carol actioned, i think this is complete
 
-const { Event } = require("../models");
+const { Event } = require('../models');
 
 const eventData = [
   {
@@ -8,35 +8,40 @@ const eventData = [
     description: "Jack's 50th Birthday Bash",
     date: 'December 21, 2021 20:30:00',
     user_id: 2,
-    city_id: 1
+    city_id: 1,
+    category_id: 1,
   },
   {
     id: 2,
     description: "Peter's BBQ",
     date: 'December 15, 2021 14:30:00',
     user_id: 1,
-    city_id: 1
+    city_id: 1,
+    category_id: 1,
   },
   {
     id: 3,
     description: '21 & Over Pub Crawl',
     date: 'December 21, 2021 20:30:00',
     user_id: 4,
-    city_id: 4
+    city_id: 4,
+    category_id: 3,
   },
   {
     id: 4,
     description: "Kim's Sunset Drinks",
     date: 'December 31, 2021 18:00:00',
     user_id: 5,
-    city_id: 11
+    city_id: 11,
+    category_id: 9,
   },
   {
     id: 5,
     description: "Justin's Breakfast Jog",
     date: 'January 10, 2022 06:30:00',
     user_id: 7,
-    city_id: 2
+    city_id: 2,
+    category_id: 10,
   },
 ];
 
